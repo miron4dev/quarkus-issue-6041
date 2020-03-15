09019bc9477a0fb282cc296f454409ee1ef66b55
miron4dev <miron4dev@gmail.com>
2020-03-15 15:57:22 +0200

    Add log message to RegisterCustomModuleCustomizer

--- src/main/kotlin/com/miron4dev/RegisterCustomModuleCustomizer.kt
+++ src/main/kotlin/com/miron4dev/RegisterCustomModuleCustomizer.kt
@@ -3,12 +3,18 @@ package com.miron4dev
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import io.quarkus.jackson.ObjectMapperCustomizer
+import org.slf4j.LoggerFactory
 import javax.inject.Singleton
 
 @Singleton
 class RegisterCustomModuleCustomizer: ObjectMapperCustomizer {
 
     override fun customize(objectMapper: ObjectMapper) {
+        logger.info("Registering Kotlin extension")
         objectMapper.registerModule(KotlinModule())
+    }
+
+    companion object {
+        private val logger = LoggerFactory.getLogger(RegisterCustomModuleCustomizer::class.java)
     }
 }
